6dd69c716c6ec3a7d17be10faa1fb5dcc05fbffc
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2024-08-31 12:22:14 +0000

    feat: Add redis dependency and store messages in Redis
    This commit adds the redis dependency and modifies the package.json and package-lock.json files to include the redis package with version 4.7.0. It also introduces changes to the send.ts file, where messages are now stored in Redis using the redis client. The messages are associated with a user's name and have an expiry time of 24 hours.

---
+++ pages/api/send.ts
@@ -0,0 +1,37 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { generateSlug } from "random-word-slugs";
+import jsonwebtoken from "jsonwebtoken";
+import redis from "redis";
+import { promisify } from "util";
+
+const client = redis.createClient({
+  url: process.env.REDIS_URL,
+});
+
+type Data = {
+  ack: boolean;
+  status: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  //get token, message from the request
+    const { token, message } = req.body;
+    // verify the token
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      throw new Error("JWT_SECRET is not set");
+    }
+    const payload = jsonwebtoken.verify(token, secret);
+    // store the message in redis
+    //@ts-ignore
+    const setdata = client.set(payload.name, message);
+    // set the expiry time for the key
+    //@ts-ignore
+    const expire = client.expire(payload.name, 60*60*60*24);
+    // send the response
+    res.status(200).json({ ack: true, status: "Message sent" });
+}
